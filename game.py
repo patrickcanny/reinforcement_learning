3ec309c88992393abd2945817debeb4020af69fd
Liam Ormiston <ljormiston@ku.edu>
2019-05-13 20:15:46 -0500

    needed to fix indention for game

--- game.py
+++ game.py
@@ -135,42 +135,42 @@ class Qmaze(object):
         return actions
 
     def show(qmaze):
-    plt.grid('on')
-    nrows, ncols = qmaze.maze.shape
-    ax = plt.gca()
-    ax.set_xticks(np.arange(0.5, nrows, 1))
-    ax.set_yticks(np.arange(0.5, ncols, 1))
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    canvas = np.copy(qmaze.maze)
-    for row,col in qmaze.visited:
-        canvas[row,col] = 0.6
-    rat_row, rat_col, _ = qmaze.state
-    canvas[rat_row, rat_col] = 0.3   # rat cell
-    canvas[nrows-1, ncols-1] = 0.9 # cheese cell
-    img = plt.imshow(canvas, interpolation='none', cmap='gray')
-    return img
+        plt.grid('on')
+        nrows, ncols = qmaze.maze.shape
+        ax = plt.gca()
+        ax.set_xticks(np.arange(0.5, nrows, 1))
+        ax.set_yticks(np.arange(0.5, ncols, 1))
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        canvas = np.copy(qmaze.maze)
+        for row,col in qmaze.visited:
+            canvas[row,col] = 0.6
+        rat_row, rat_col, _ = qmaze.state
+        canvas[rat_row, rat_col] = 0.3   # rat cell
+        canvas[nrows-1, ncols-1] = 0.9 # cheese cell
+        img = plt.imshow(canvas, interpolation='none', cmap='gray')
+        return img
 
     def play_game(model, qmaze, rat_cell):
-    qmaze.reset(rat_cell)
-    envstate = qmaze.observe()
-    while True:
-        prev_envstate = envstate
-        # get next action
-        q = model.predict(prev_envstate)
-        action = np.argmax(q[0])
+        qmaze.reset(rat_cell)
+        envstate = qmaze.observe()
+        while True:
+            prev_envstate = envstate
+            # get next action
+            q = model.predict(prev_envstate)
+            action = np.argmax(q[0])
 
-        # apply action, get rewards and new state
-        envstate, reward, game_status = qmaze.act(action)
-        if game_status == 'win':
-            return True
-        elif game_status == 'lose':
-            return False
+            # apply action, get rewards and new state
+            envstate, reward, game_status = qmaze.act(action)
+            if game_status == 'win':
+                return True
+            elif game_status == 'lose':
+                return False
 
     def completion_check(model, qmaze):
-    for cell in qmaze.free_cells:
-        if not qmaze.valid_actions(cell):
-            return False
-        if not play_game(model, qmaze, cell):
-            return False
-    return True
+        for cell in qmaze.free_cells:
+            if not qmaze.valid_actions(cell):
+                return False
+            if not play_game(model, qmaze, cell):
+                return False
+        return True
